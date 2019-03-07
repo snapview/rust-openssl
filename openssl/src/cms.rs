823e52cbe68f1572dd17e3719803536730e85f33
Alexey Galakhov <agalakhov@snapview.de>
2019-03-07 03:50:19 +0100

    CMS: add verify method

--- openssl/src/cms.rs
+++ openssl/src/cms.rs
@@ -15,8 +15,9 @@ use libc::c_uint;
 use pkey::{HasPrivate, PKeyRef};
 use stack::StackRef;
 use x509::{X509Ref, X509};
+use x509::store::X509Store;
 use symm::Cipher;
-use {cvt, cvt_p};
+use {cvt, cvt_n, cvt_p};
 
 bitflags! {
     pub struct CMSOptions : c_uint {
@@ -93,6 +94,54 @@ impl CmsContentInfoRef {
             ))?;
 
             Ok(out.get_buf().to_owned())
+        }
+    }
+
+    /// Verify the sender's signature given an optional sender's certificate `cert` and CA store
+    /// `store`. If the signature is a detached one, `data` is used for the input of signed data.
+    /// If `output_data` is not `None`, the signed data is written to it.
+    ///
+    /// OpenSSL documentation at [`CMS_verify`]
+    /// 
+    /// [`CMS_verify`]: https://www.openssl.org/docs/manmaster/man3/CMS_verify.html
+    pub fn verify(
+        &self,
+        certs: Option<&StackRef<X509>>,
+        store: Option<&X509Store>,
+        data: Option<&[u8]>,
+        output_data: Option<&mut Vec<u8>>,
+        flags: CMSOptions,
+    ) -> Result<bool, ErrorStack> {
+        unsafe {
+            let certs = match certs {
+                Some(certs) => certs.as_ptr(),
+                None => ptr::null_mut(),
+            };
+            let store = match store {
+                Some(store) => store.as_ptr(),
+                None => ptr::null_mut(),
+            };
+            let in_data = match data {
+                Some(data) => MemBioSlice::new(data)?.as_ptr(),
+                None => ptr::null_mut(),
+            };
+            let out_bio = MemBio::new()?;
+
+            let is_valid = cvt_n(ffi::CMS_verify(
+                self.as_ptr(),
+                certs,
+                store,
+                in_data,
+                out_bio.as_ptr(),
+                flags.bits(),
+            ))? == 1;
+
+            if let Some(out_data) = output_data {
+                out_data.clear();
+                out_data.extend_from_slice(out_bio.get_buf());
+            };
+
+            Ok(is_valid)
         }
     }
 
@@ -228,6 +277,7 @@ mod test {
     use super::*;
     use stack::Stack;
     use x509::X509;
+    use x509::store::X509StoreBuilder;
     use pkcs12::Pkcs12;
 
     #[test]
@@ -266,5 +316,36 @@ mod test {
             let decrypt = String::from_utf8(decrypt).expect("failed to create string from cms content");
             assert_eq!(input, decrypt);
         }
+    }
+
+    #[test]
+    fn cms_sign_verify() {
+        // load cert with private key
+        let priv_cert_bytes = include_bytes!("../test/cms.p12");
+        let priv_cert = Pkcs12::from_der(priv_cert_bytes).expect("failed to load priv cert");
+        let priv_cert = priv_cert.parse("mypass").expect("failed to parse priv cert");
+
+        // sign cms message using private key cert
+        let input = String::from("My Message");
+        let sign = CmsContentInfo::sign(Some(&priv_cert.cert), Some(&priv_cert.pkey), None, Some(&input.as_bytes()), CMSOptions::empty())
+            .expect("failed create signed cms");
+        let sign = sign.to_der().expect("failed to create der from cms");
+
+        // verify signature on cms message
+        let verify = CmsContentInfo::from_der(&sign).expect("failed read cms from der");
+
+        let mut cert_stack = Stack::new().expect("failed to create stack");
+        cert_stack.push(priv_cert.cert.clone()).expect("failed to add cert to stack");
+
+        let mut store_builder = X509StoreBuilder::new().expect("failed to create store builder");
+        store_builder.add_cert(priv_cert.cert.clone()).expect("failed to add certificate to store");
+        let store = store_builder.build();
+
+        let mut data = Vec::new();
+        let verify = verify.verify(Some(&cert_stack), Some(&store), None, Some(&mut data), CMSOptions::empty()).expect("failed to verify cms");
+        assert!(verify);
+        let verify = String::from_utf8(data).expect("failed to create string from cms content");
+
+        assert_eq!(input, verify);
     }
 }
